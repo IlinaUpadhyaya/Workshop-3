48282cb130a468ac23342cec2b2d26439e4eef8b
IlinaUpadhyaya <ilina.upadhyaya@outlook.com>
2022-08-08 17:51:31 +1000

    part_2
    completed part 2

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -35,8 +35,11 @@ public class Scene : MonoBehaviour
 
         // Image plane "corner" rays first (frustum edges).
         this.debug.Ray(new Ray(Vector3.zero, NormalizedImageToWorldCoord(0f, 0f)), Color.blue);
-        
+
         // Add more rays to visualise here...
+        this.debug.Ray(new Ray(Vector3.zero, NormalizedImageToWorldCoord(1f, 0f)), Color.blue);
+        this.debug.Ray(new Ray(Vector3.zero, NormalizedImageToWorldCoord(0f, 1f)), Color.blue);
+        this.debug.Ray(new Ray(Vector3.zero, NormalizedImageToWorldCoord(1f, 1f)), Color.blue);
     }
 
     private void Render()
